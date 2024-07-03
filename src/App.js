d04700d1d1297c0b9e55e6580bee179af9afe8dd
Karanme390 <karanme390@gmail.com>
2024-07-03 15:49:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import CTO from './components/CTO';
+import CEO from './components/CEO';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Understanding Routing Hooks</h1>
+      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="about" element={<AboutUs/>}/>
+        <Route path="contact" element={<ContactUs/>}/>
+        <Route path="ceo" element={<CEO/>}/>
+        <Route path="cto" element={<CTO/>}/>
+
+
+      </Routes>
+      <h1>Thanks for visiting</h1>
     </div>
   );
 }
